8567a3ec8248bc5da64e0a2653c2788a0282b3ad
djtemple <djtemple9@gmail.com>
2017-01-27 20:38:39 -0700

    m

--- src/kernel/syscalls.cc
+++ src/kernel/syscalls.cc
@@ -131,7 +131,7 @@ extern "C" int sched_setaffinity(pid_t pid, size_t cpusetsize, cpu_set_t *mask) 
   if(b >= 16){
     return EINVAL;
   }
-  Runtime::getCurrThread()->setAffinityMask(mword(mask));
+  LocalProcessor::getCurrThread()->setAffinityMask(*mask);
   LocalProcessor::getScheduler()->yield();
 
   return 0;
