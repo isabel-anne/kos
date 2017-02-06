d915ce577dda5771aa095d20db6c98e3dcdb225b
isabel-anne <isabel-anne@live.ca>
2017-02-06 16:44:32 -0700

    .

--- src/kernel/Kernel.cc
+++ src/kernel/Kernel.cc
@@ -94,8 +94,8 @@ void kosMain() {
   // Scheduler::defaultEpochLength = Scheduler::defaultEpochLength*(Machine::cyclesPerSecond/1000);
   // Scheduler::schedMinGranularity = Scheduler::schedMinGranularity*(Machine::cyclesPerSecond/1000);
   // KOUT::outl("cyclesPerSecond = %d", Machine::cyclesPerSecond);
-  KOUT::outl("Schedule Parameter\n defaultEpochLength = ", Scheduler::defaultEpochLength);
-  KOUT::outl("MinGranularity = %d", Scheduler::schedMinGranularity);
+  KOUT::outl("Schedule Parameter after PARSE\n defaultEpochLength = ", Scheduler::defaultEpochLength);
+  KOUT::outl("MinGranularity = ", Scheduler::schedMinGranularity);
 
   /*end editted by Isabel*/
 
