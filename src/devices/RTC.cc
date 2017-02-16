bebe65a2c11aa729222878f3833b3252a9e90896
isabel-anne <isabel-anne@live.ca>
2017-02-16 16:08:41 -0700

    added tree.h file

--- src/devices/RTC.cc
+++ src/devices/RTC.cc
@@ -27,11 +27,11 @@ void RTC::init() { // see http://wiki.osdev.org/RTC
   CPU::out8(0x70, 0x0A);             // select Status Register A
   uint8_t prev = CPU::in8(0x71);     // read current value
   CPU::out8(0x70, 0x0A);             // select Status Register A
-//edited by Isabel
+//edited
 //  CPU::out8(0x71, prev | 0x06);      // set rate to 32768 / (2^(6-1)) = 1024 Hz
   CPU::out8(0x71, prev | 0x04);      // set rate to 32768 / (2^(4-1)) = 4096 Hz
   KOUT::out1("RTC interupt rate is 4096 Hz\n");
-//end edited by Isabel
+//end edited
   CPU::out8(0x70, 0x0B);             // select Status Register B
   prev = CPU::in8(0x71);             // read current value
   CPU::out8(0x70, 0x0B);             // select Status Register B
